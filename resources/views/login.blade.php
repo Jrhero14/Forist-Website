5d5305dc34df0a6727da771cb29685e49da6be90
Jeremi Herodian Abednigo <43849393+Jrhero14@users.noreply.github.com>
2024-01-19 00:38:38 +0800

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -67,7 +67,45 @@
 
 <script src="https://kit.fontawesome.com/f51741a9dd.js" crossorigin="anonymous"></script>
 @vite('resources/js/preline.js')
-@vite('resources/js/sweetalert.all.js')
+@if (Session::has('alert.config') || Session::has('alert.delete'))
+    @if (config('sweetalert.animation.enable'))
+        <link rel="stylesheet" href="{{ config('sweetalert.animatecss') }}">
+    @endif
+
+    @if (config('sweetalert.theme') != 'default')
+        <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11" rel="stylesheet">
+    @endif
+
+    @if (config('sweetalert.alwaysLoadJS') === false && config('sweetalert.neverLoadJS') === false)
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    @endif
+    <script>
+        @if (Session::has('alert.delete'))
+        document.addEventListener('click', function(event) {
+            if (event.target.matches('[data-confirm-delete]')) {
+                event.preventDefault();
+                Swal.fire({!! Session::pull('alert.delete') !!}).then(function(result) {
+                    if (result.isConfirmed) {
+                        var form = document.createElement('form');
+                        form.action = event.target.href;
+                        form.method = 'POST';
+                        form.innerHTML = `
+                    @csrf
+                        @method('DELETE')
+                        `;
+                        document.body.appendChild(form);
+                        form.submit();
+                    }
+                });
+            }
+        });
+        @endif
+
+        @if (Session::has('alert.config'))
+        Swal.fire({!! Session::pull('alert.config') !!});
+        @endif
+    </script>
+@endif
 </body>
 
 </html>
